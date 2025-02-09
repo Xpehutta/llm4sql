5796a0c638308c09f1d9ba08db16ac1d7ec49e63
Xpehutta <42118488+Xpehutta@users.noreply.github.com>
2025-02-09 09:49:54 +0300

    Update example_usage.py

--- data-model-generator/examples/example_usage.py
+++ data-model-generator/examples/example_usage.py
@@ -1,6 +1,8 @@
 from src.data_model_generator import DataModelGenerator
+from model.data_model_generator import create_data_model_excel
 
 if __name__ == "__main__":
+    create_data_model_excel()
     generator = DataModelGenerator()
     generator.load_data_model()
     dataset, queries = generator.generate_dataset(num_queries=5000)
